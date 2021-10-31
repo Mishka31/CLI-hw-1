caf78499ae7bac13c0cd0c975ad5024d5e61d81b
Misha Krasnonos <muzishutka7@gmail.com>
2021-10-31 20:09:46 +0200

    up

--- base/main.cjs
+++ base/main.cjs
@@ -6,8 +6,9 @@ console.log(__dirname);
 // console.log(process.argv);
 
 console.log(process.cwd());
-process;.on('exit', (code) =>{
-    console.log(code);
-})
+process.on("exit", (code) => {
+  console.log(code);
+});
 
-process.exit(101)
+process.exit(101);
+console.log("End");
---
+++ files/rw.js
@@ -0,0 +1,12 @@
+const fs = require("fs/promises");
+const file = "../base/main.mjs";
+
+(async (fileName) => {
+  const file = await fs;
+})(file);
+
+const Obj = {
+  sadf: "sdf",
+  sdffs: "sdf",
+};
+Obj;
