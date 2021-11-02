9f6f77acb06877ee0073fbffd37406944694eb83
Misha Krasnonos <muzishutka7@gmail.com>
2021-11-02 02:44:43 +0200

    up

--- files/rw.js
+++ files/rw.js
@@ -3,7 +3,7 @@ const file = "../base/main.mjs";
 
 (async (fileName) => {
   const file = await fs.readFile(fileName, 'utf8')
-  console.log(file);
-  await fs.mkdir('./temp')
+  // console.log(file);
+  await fs.mkdir('./temp', {recursive: true})
   await fs.writeFile('./temp/temp.js', `${file}console.log('Hello word')`) 
 })(file);
